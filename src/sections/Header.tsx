18aa3ae988504c510eb5da10154590af83819c62
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-02 00:08:50 +0300

    initial clean commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div className="flex justify-center items-center relative top-3" >
+      <nav>
+        <a href="#" className="px-4 py-1.5 round" >Home</a>
+        <a href="#">Projects</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+    </div>
+  );
+};
