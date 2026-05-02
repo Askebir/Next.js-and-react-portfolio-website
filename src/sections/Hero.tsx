f7250f9e0161890a6cb25860ab0e7b368182602c
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-03 00:29:49 +0300

    hero section without fancycy background

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,45 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-24">
+      <div className="container">
+        <div className="flex flex-col  items-center  ">
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="Person peeking from behind laptop"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-6  rounded-lg ">
+            <div className="bg-green-500  size-2.5  rounded-full "></div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+        </div>
+        <h1 className="font-serif text-3xl text-center mt-9  tracking-wider ">
+          Building Exceptional User Experiences
+        </h1>
+        <p className="mt-4 text-center text-white/60 ">
+          Ispecialize in transforimg designs into functional, high-performing
+          Web applcation. Let's discuss your next project.{" "}
+        </p>
+        <div className="flex flex-col items-center mt-10 gap-4  ">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Works</span>
+            <span className="w-4 h-4">
+              {" "}
+              <ArrowDown />
+            </span>
+          </button>
+          <button className="inline-flex items-center gap-6 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl ">
+            <span> 👋</span>
+            <span className="font-semibold ">Let's Conect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
