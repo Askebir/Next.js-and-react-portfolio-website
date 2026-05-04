8618dc8c3726659d61b1d1dba3bca181b312502a
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-04 23:58:04 +0300

    hero-ring ui

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,22 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-24 md:py-lg lg:py-28 ">
+    <div className="py-24 md:py-lg lg:py-28 relative z-0 ">
+      b
+      <div
+        className="absolute inset-0 -z-30 opacity-5 "
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      ></div>
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
       <div className="container">
         <div className="flex flex-col  items-center  ">
           <Image
