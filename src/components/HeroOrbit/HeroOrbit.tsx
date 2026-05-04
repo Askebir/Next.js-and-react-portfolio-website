8c4249145cd2f6db5e78134bd5488faf30a1d5a0
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-05 00:35:56 +0300

    star rotaion animation intiation

---
+++ src/components/HeroOrbit/HeroOrbit.tsx
@@ -0,0 +1,34 @@
+import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
+import { PropsWithChildren } from "react";
+import { transform } from "next/dist/build/swc";
+
+const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  return (
+    <div className="absolute left-1/2 top-1/2  -translate-x-1/2 -translate-y-1/2 ">
+      <div
+        className=" border border-red-500 "
+        style={{
+          transform: `rotate(${rotation}deg)`,
+          height: `${size}px`,
+          width: `${size}px`,
+        }}
+      >
+        <div
+          className="border border-red-500 inline-flex  "
+          style={{
+            transform: `rotate(${rotation * -1}deg)`,
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroOrbit;
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,11 +2,12 @@ import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
+import HeroOrbit from "@/components/HeroOrbit/HeroOrbit";
 
 export const HeroSection = () => {
   return (
-    <div className="py-24 md:py-lg lg:py-28 relative z-0 ">
-      b
+    <div className="py-24 md:py-lg lg:py-28 relative z-0 overflow-x-clip ">
       <div
         className="absolute inset-0 -z-30 opacity-5 "
         style={{
@@ -17,6 +18,11 @@ export const HeroSection = () => {
       <div className="size-[820px] hero-ring"></div>
       <div className="size-[1020px] hero-ring"></div>
       <div className="size-[1220px] hero-ring"></div>
+
+      <HeroOrbit size={800} rotation={45}>
+        <StarIcon className="size-12 text-emerald-300" />
+      </HeroOrbit>
+
       <div className="container">
         <div className="flex flex-col  items-center  ">
           <Image
