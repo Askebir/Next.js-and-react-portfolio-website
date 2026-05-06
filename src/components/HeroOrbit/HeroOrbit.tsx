267c42f1d05e742a777d30019068ddf5b79d8b89
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-06 21:19:43 +0300

    sstars and the hero bottom fade out design

--- src/components/HeroOrbit/HeroOrbit.tsx
+++ src/components/HeroOrbit/HeroOrbit.tsx
@@ -11,7 +11,7 @@ const HeroOrbit = ({
   return (
     <div className="absolute left-1/2 top-1/2  -translate-x-1/2 -translate-y-1/2 ">
       <div
-        className=" border border-red-500 "
+        className="  "
         style={{
           transform: `rotate(${rotation}deg)`,
           height: `${size}px`,
@@ -19,7 +19,7 @@ const HeroOrbit = ({
         }}
       >
         <div
-          className="border border-red-500 inline-flex  "
+          className=" inline-flex  "
           style={{
             transform: `rotate(${rotation * -1}deg)`,
           }}
