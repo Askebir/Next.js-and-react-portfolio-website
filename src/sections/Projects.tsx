44ceaabad2cd36300832b25a0ab3fcc037620696
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-13 00:38:13 +0300

    half of pr project section styling

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,6 +2,9 @@ import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
@@ -44,35 +47,65 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <div>
-      <div className="container border border-white mb-16 ">
-        <p className="uppercase font-semibold font-semibold ">
-          Real-world Results
+    <section className="pb-16">
+      <div className="container  rounded-l ">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent  ">
+            Real-world Results
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6 ">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4  ">
+          {" "}
+          See how I transformed concepts into engaging digital experiences.
         </p>
-        <h2>Featured Projects</h2>
-        <p>See how I transformed concepts into engaging digital experiences.</p>
-        <div>
+        <div className="flex flex-col mt-10 gap-20 ">
           {portfolioProjects.map((project) => (
-            <div key={project.title}>
-              <div>
-                <div>{project.company}</div>
-                <div>{project.year}</div>
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative z-0 after:z-10 after:content-[''] after:absolute overflow-hidden after:inset-0 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20  px-8  pt-8 "
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5 "
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              ></div>
+              <div className="flex  ">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold gap-2 text-transparent bg-clip-text  uppercase tracking-widest text-sm ">
+                  <span>{project.company}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
               </div>
-              <h3>{project.title}</h3>
-              <hr />
-              <ul>
+              <h3 className="font-serif text-2xl mt-2  ">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4" />
+              <ul className="flex flex-col gap-4 mt-4 ">
                 {project.results.map((result) => (
-                  <li>{result.title}</li>
+                  <li className="flex gap-2 text-sm text-white/50  ">
+                    {" "}
+                    <CheckCircleIcon className="size-5" />{" "}
+                    <span>{result.title}</span>
+                  </li>
                 ))}
               </ul>
               <a href={project.link}>
-                <button>View Live Site</button>
+                <button className="bg-white text-gray-950  h-12 w-full rounded-xl font-semibold inline-flex itmes-center justify-center pt-2.5  gap-2 mt-8  ">
+                  <span>Visit Live Site</span>
+                  <ArrowUpRightIcon className="size-4" />
+                </button>
               </a>
-              <Image src={project.image} alt={project.title} />
+              <Image
+                src={project.image}
+                alt={project.title}
+                className="mt-8 -mb-4 "
+              />
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
