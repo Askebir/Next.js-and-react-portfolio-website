4f0c3e9b7c4d1962062a30b13c690683a28147a3
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-16 23:57:01 +0300

    try and error to understand mask-image of tailwind css

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -51,15 +51,29 @@ export const TestimonialsSection = () => {
           description="Don't just take my word for it. See what my clients have to say about my
         work."
         />
-        <div>
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name}>
-              <Image src={testimonial.avatar} alt={testimonial.name} />
-              <div>{testimonial.name}</div>
-              <div>{testimonial.position}</div>
-              <p>{testimonial.text}</p>
-            </Card>
-          ))}
+        <div className="mt-16 flex  overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]  ">
+          <div className="flex flex-none gap-8 ">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs">
+                <div className="flex gap-4 items-center ">
+                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center  ">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-w-full"
+                    />
+                  </div>
+                  <div className="">
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-4 text-sm">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
