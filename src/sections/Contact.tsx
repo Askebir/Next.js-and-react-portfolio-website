29d5b78588498264f537fc4cdb079ac94c871773
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-20 04:53:53 +0300

    contact section completion

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,17 +1,37 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 export const ContactSection = () => {
   return (
-    <div>
-      <h2>Let's create someting amezing together</h2>
-      <p>
-        Ready to bring your next project to life? Let's connect and discuss how
-        I can help you achieve your goals.
-      </p>
-      <button>
-        <span> Contact Me</span>
-        <ArrowUpRightIcon />
-      </button>
+    <div className="py-16 pt-14 lg:py-24 lg:pt-20 ">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center relative overflow-hidden z-0 ">
+          <div
+            className="absolute inset-0 opacity-5 -z-20"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <div className="flex flex-col gap-8 md:gap-16 items-center md:flex-row md:text-left ">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Let's create someting amezing together
+              </h2>
+
+              <p className="text-sm mt-2 md:text-base ">
+                Ready to bring your next project to life? Let's connect and
+                discuss how I can help you achieve your goals.
+              </p>
+            </div>
+            <div className="">
+              <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8 w-max border border-gray-900">
+                <span className="font-semibold"> Contact Me</span>
+                <ArrowUpRightIcon className="size-4 " />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
